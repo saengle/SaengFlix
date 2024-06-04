aa777971d1fffe7ec91645031699ca5b51a1a1be
saengle <zohv817@gmail.com>
2024-06-04 19:25:11 +0900

    [Add] Mainview, MainViewController

---
+++ SaengFlix/Main/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  SaengFlix
+//
+//  Created by 쌩 on 6/4/24.
+//
+
+import UIKit
+
+import SnapKit
+
+class MainView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- SaengFlix/Main/MainViewController.swift
+++ SaengFlix/Main/MainViewController.swift
@@ -6,3 +6,14 @@
 //
 
 import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension MainViewController {
+    
+}
